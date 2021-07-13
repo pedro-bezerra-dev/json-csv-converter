0dd4dfeb3ce02023631faf0058d62860eae07955
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 23:06:29 -0300

    Main styled component imported in the Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { Header } from '../components/Header';
+import { Main } from '../components/Main';
 import { Footer } from '../components/Footer';
 
 export function Home() {
   return (
     <>
       <Header />
+      <Main>
+        Hello
+      </Main>
       <Footer />
     </>
   );
