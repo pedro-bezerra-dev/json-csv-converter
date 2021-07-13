727945e7175425a411c411c679b41cac0dafeb92
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 22:26:47 -0300

    Header component finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,25 @@
 import { HeaderStyled } from './styles';
 
+import logoImg from '../../assets/images/logo.png';
+import githubImg from '../../assets/images/github.svg';
+import linkedinImg from '../../assets/images/linkedin.svg';
+import instagramImg from '../../assets/images/instagram.svg';
+
 export function Header() {
   return (
     <HeaderStyled>
-      Hello World
+      <img src={logoImg} alt="Logo" />
+      <div>
+        <a href="https://github.com/pedro-henrique-sb/">
+          <img src={githubImg} alt="GitHub icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/pedro-henrique-sb/">
+          <img src={linkedinImg} alt="Linkedin icon" />
+        </a>
+        <a href="https://www.instagram.com/pedro_henrique_silva_b/">
+          <img src={instagramImg} alt="Instagram icon" />
+        </a>
+      </div>
     </HeaderStyled>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,36 @@
 import styled from 'styled-components';
 
 export const HeaderStyled = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+  width: 100%;
+  border: 1px solid ${(props) => props.theme.colors.primary};
+  padding: 24px 100px;
+
+  > img {
+    width: 48px;
+    display: inline-block;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    gap: 15px;
+
+    a {
+      width: 36px;
+      border-radius: 5px;
+
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 5px;
+      transition: 0.2s;
+
+      &:hover {
+        background: rgba(63, 132, 229, .3);
+      }
+    }
+  }
 `;
