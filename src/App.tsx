f2f94381e4b8fbb358083e7d0ee091e3395172b8
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 21:27:55 -0300

    GlobalStyles and ThemeProvider added in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components';
+import { theme, GlobalStyle } from './styles/globals';
+
 export function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <div className="App">
+        Hello World
+      </div>
+    </ThemeProvider>
   );
 }
