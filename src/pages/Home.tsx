c77d448d4a116fccf601942808d5cddb0fe679e8
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 22:38:51 -0300

    Footer component imported in the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 export function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
