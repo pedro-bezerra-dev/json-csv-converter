deb0572167999d999d4e854e6582b974a08d8d4b
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 21:56:48 -0300

    Header component imported

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
+import { Header } from '../components/Header';
+
 export function Home() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <Header />
     </div>
   );
 }
