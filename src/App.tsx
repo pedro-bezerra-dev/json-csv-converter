15253bedbc61488b75defbca9aca2dc3942b67fa
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 21:48:14 -0300

    Home component added in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ThemeProvider } from 'styled-components';
+
+import { Home } from './pages/Home';
+
 import { theme, GlobalStyle } from './styles/globals';
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div className="App">
-        Hello World
-      </div>
+      <Home />
     </ThemeProvider>
   );
 }
