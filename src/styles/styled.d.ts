f3ccb5abe340aff6208f0ba367606777f71e89ff
Pedro Henrique <pyter.dev@gmail.com>
2021-07-12 21:40:56 -0300

    types of the default theme created

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,15 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      background: string;
+      primary: string;
+      secondary: string;
+    }
+    text: {
+      codeText: string;
+      title: string;
+    }
+  }
+}
